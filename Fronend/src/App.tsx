2832d34e6b2ffaa42c3629f63a30f29ce2f18860
alzubair <zubairalojali@gmail.com>
2026-02-03 20:31:29 +0200

    install all important package

--- Fronend/src/App.tsx
+++ Fronend/src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='bg-red-500 text-white p-5'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
